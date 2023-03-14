1cda6e70cefba9a29fd252950929e36a18bdcf8c
Chandu <chandrashekarumapathi@gmail.com>
2023-03-14 12:38:20 +0100

    updated tests

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from main import Calculator
 
+# todo: write a proper test
 for i in range(5):
     eg = Calculator(input1=float(input("Enter a number:")), input2=float(input("Enter another number: ")),
                     choice=input("Enter the desired operation: "))
